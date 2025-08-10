3af140a18749429a0326f8c947e68510ab77ed78
Angela L <heiyan@Angelas-MacBook-Pro.local>
2025-08-10 13:58:54 -0700

    narrow border

--- router.py
+++ router.py
@@ -13,6 +13,11 @@ def inject_custom_css():
         * {
             border-radius: 0 !important;
         }
+                
+        div.block-container {
+        padding-top: 1rem !important;
+        padding-bottom: 0rem !important;
+    }
         </style>
     """, unsafe_allow_html=True)
 
